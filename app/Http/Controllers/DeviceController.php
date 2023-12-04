696ad5b19b8a9a4fc7562af4b83bda35df0227f9
Mustafa Kilic <mustafa.kilic@tirport.com>
2023-12-04 17:41:15 +0300

    Edited device controller and added swagger descriptions.

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -1,42 +1,64 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Str;
 use App\Models\Device;
-use Illuminate\Http\Request;
+
 class DeviceController extends Controller
 {
-    public function deviceLogin(Request $request)
-    {
-        $request->validate([
-            'deviceUuid' => ['required', 'string', function ($attribute, $value, $fail) {
-                if (!Str::isUuid($value)) {
-                    $fail($attribute . ' is not a valid UUID.');
-                }
-            }],
-        ]);
+    /**
+     * @OA\Post(
+     *     path="/api/device/login",
+     *     operationId="deviceLogin",
+     *     tags={"Device"},
+     *     summary="Device login",
+     *     description="Login or create a device",
+     *     @OA\RequestBody(
+     *         required=false,
+     *         @OA\JsonContent(
+     *             @OA\Property(property="deviceUuid", type="string", example="123e4567-e89b-12d3-a456-426614174001"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Device logged in successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="uuid", type="string", example="generated_uuid_here"),
+     *             @OA\Property(property="premium_status", type="boolean", example=false),
+     *             @OA\Property(property="config_info", type="object", example=null),
+     *         ),
+     *     ),
+     * )
+     */
 
-        $device = Device::where('uuid', $request->input('deviceUuid'))->first();
-        if ($device) {
-            $accessToken = $device->createToken('authToken', ['deviceId'])->accessToken;
-            return response()->json([
-                'access_token' => $accessToken,
-                'premium_status' => $device->premium_status,
-                'config_info' => $device->config_info,
-            ]);
-        } else {
-            $newDevice = Device::create([
+
+    public function deviceLogin(Request $request): JsonResponse
+    {
+        $deviceUuid = $request->input('deviceUuid');
+
+        if (!$deviceUuid) {
+            $device = Device::create([
                 'uuid' => Str::uuid(),
                 'premium_status' => false,
                 'config_info' => null,
             ]);
 
-            $accessToken = $newDevice->createToken('authToken', [$newDevice->uuid])->accessToken;
-            return response()->json([
-                'access_token' => $accessToken,
-                'premium_status' => $newDevice->premium_status,
-                'config_info' => $newDevice->config_info,
-            ]);
+        } else {
+            $device = Device::where('uuid', $deviceUuid)->first();
+            if (!$device) {
+                return response()->json(['error' => 'Device not found'], 404);
+            }
         }
+        $accessToken = $device->createToken('authToken', ['uuid' => $device->uuid])->plainTextToken;
+        $response = [
+            'uuid' => $device->uuid,
+            'premium_status' => $device->premium_status,
+            'config_info' => $device->config_info,
+            'access_token' => $accessToken,
+        ];
+
+        return response()->json($response);
     }
 }
