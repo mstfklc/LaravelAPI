9d332fda188acff0b87865c78b8e7a20f908aeed
Mustafa Kilic <mustafa.kilic@tirport.com>
2023-12-04 20:22:36 +0300

    Forgotten models deleted on SubscriptionController.

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\OrderHistory;
 use App\Models\Product;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
