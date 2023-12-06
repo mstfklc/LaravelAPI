15da9ceb1dd8cce1f31bc36e23eca56d8a623d20
Mustafa Kilic <mustafa.kilic@tirport.com>
2023-12-06 22:37:18 +0300

    Edited in routes.

--- routes/api.php
+++ routes/api.php
@@ -6,20 +6,13 @@ use App\Http\Controllers\SubscriptionController;
 use App\Http\Controllers\AdminController;
 
 
-Route::group(['middleware' => ['guest']], function () {
-    Route::post('device/login', [DeviceController::class, 'deviceLogin']);
-});
 
-Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::post('purchase/product', [SubscriptionController::class, 'purchaseProduct']);
-});
-
+Route::post('device/login', [DeviceController::class, 'deviceLogin'])->middleware('guest');
+Route::post('purchase/product', [SubscriptionController::class, 'purchaseProduct'])->middleware('auth:sanctum');
 Route::group(['Admin' => 'api','prefix'=>'admin'], function () {
     Route::post('register', [AdminController::class, 'adminRegister']);
-    Route::post('login', [AdminController::class, 'adminLogin']);
 });
 
-Route::group(['middleware' => ['auth:sanctum'],'prefix'=>'admin'], function () {
-    Route::get('list-order', [AdminController::class, 'listOrderHistory']);
-});
+
+
 
